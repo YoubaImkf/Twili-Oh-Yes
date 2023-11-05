f733c61917bec9ec4ba7b3ed93b6adb7a6620aae
kyoxdev <mevshypocrisie@yahoo.com>
2023-11-05 21:30:02 +0100

    🔥  Delete files

--- src/message/messageInterface.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from './message';
-
-export interface MessageInterface {
-    GetById(id: number): Message | string;
-    Add(user: Message): Message;
-    Delete(id: number): void;
-    Update(user: Message): Message;
-}
