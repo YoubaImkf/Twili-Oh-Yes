b23abf97c55230bfabdae1e5c5c5d65230ba4754
kyoxdev <mevshypocrisie@yahoo.com>
2023-12-08 20:43:53 +0100

    🎨  Improve structure of code

--- src/Twili-Oh-Yes.Web/src/components/ui/ChatHeader.vue
+++ src/Twili-Oh-Yes.Web/src/components/ui/ChatHeader.vue
@@ -5,10 +5,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'ChatHeader',
+  name: "ChatHeader",
 });
 </script>
 
@@ -19,10 +19,10 @@ export default defineComponent({
   top: 0;
   left: 0;
   width: 100%;
-  background-color: rgba(52,53,65,.9);
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1); 
-  padding: 10px; 
+  background-color: rgba(52, 53, 65, 0.9);
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px;
   z-index: 1;
-  font-size: 0.63rem
+  font-size: 0.63rem;
 }
 </style>
