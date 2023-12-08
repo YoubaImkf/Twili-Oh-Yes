8567071e18122cfecb0c591b31ff36797d23369c
kyoxdev <mevshypocrisie@yahoo.com>
2023-12-08 20:44:42 +0100

    ✨ Add the possibility to send message

--- src/Twili-Oh-Yes.Web/src/components/ui/ChatInput.vue
+++ src/Twili-Oh-Yes.Web/src/components/ui/ChatInput.vue
@@ -1,50 +1,56 @@
 <template>
   <footer>
-  <div class="chat-input-container">
-    <textarea 
-      v-model="newMessage"
-      @input="resizeTextArea"
-      @keydown.enter.prevent="createNewLine"
-      placeholder="Type a message..."
-      class="chat-input"
-      ref="textarea"
-    ></textarea>
-    <SendButton></SendButton>
-
-  </div>
-</footer>
+    <div class="chat-input-container">
+      <textarea
+        v-model="newMessage"
+        @input="resizeTextArea"
+        @keydown.enter.prevent="createNewLine"
+        placeholder="Type a message..."
+        class="chat-input"
+        ref="textarea"
+      ></textarea>
+      <SendButton @sendMessage="sendMessage"></SendButton>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import SendButton from './SendButton.vue';
+import { defineComponent, ref } from "vue";
+import SendButton from "./SendButton.vue";
+import { MessageService } from "@/services/api/MessageService";
+
+const messageService = new MessageService();
 
 export default defineComponent({
-  name: 'ChatInput',
+  name: "ChatInput",
   components: {
     SendButton,
   },
   data() {
     return {
-      newMessage: '',
+      newMessage: "",
     };
   },
   methods: {
-    createNewLine() {
-      this.newMessage += '\n';
-      this.resizeTextArea();
-    },
-    sendMessage() {
-      if (this.newMessage.trim() !== '') {
-        this.$emit('newMessage', this.newMessage);
-        this.newMessage = '';
-        this.resizeTextArea();
-      }
-    },
     resizeTextArea() {
       const textarea = this.$refs.textarea as HTMLTextAreaElement;
-      textarea.style.height = 'auto';
+      textarea.style.height = "auto";
       textarea.style.height = `${textarea.scrollHeight}px`;
+    },
+    createNewLine() {
+      this.newMessage += "\n";
+      this.resizeTextArea();
+    },
+    async sendMessage() {
+      if (this.newMessage.trim() !== "") {
+        try {
+          await messageService.sendMessageAsync(this.newMessage.trim());
+          this.newMessage = "";
+          this.resizeTextArea();
+        } catch (error: unknown) {
+          console.error("Error sending message:", error);
+        }
+      }
     },
   },
 });
@@ -57,7 +63,7 @@ footer {
   position: fixed;
   height: 80px;
   background-color: #343541e3;
-  box-shadow: 0px 0px 2px 0px rgba(0,0,0,0.3)
+  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.3);
 }
 
 .chat-input-container {
@@ -91,10 +97,9 @@ footer {
   font-size: 16px;
   border-radius: 20px;
   resize: none;
-  overflow-y: hidden; 
+  overflow-y: hidden;
   max-height: 200px;
 }
-
 
 @media (min-width: 768px) {
   .chat-input-container {
